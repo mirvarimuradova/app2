cf7ee7e6041fdac44915194635eb385618a064ab
mirvarimuradova <mirvarimuradova7@gmail.com>
2023-11-25 23:55:26 +0400

    sertlirendering

---
+++ src/component/CustomButtom.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../App.css'
+import { useState } from 'react'
+const CustomButtom = () => {
+    const [isVisible, setIsVisible] = useState(false);
+ const [item,setItem]= useState(false)
+ function textButton(){
+   setItem(!item)
+ }
+ function warnButton(){
+    alert('Warning')
+ }
+  return (
+    <div>
+      <button onClick={textButton}>text</button><br/>
+      {item &&  <p className='prg1'>
+      Nesimi’nin yaşamı hakkında bugün elimizde çok sınırlı bilgiler bulunmaktadır ve değişik kaynaklardan sağlanan bilgiler de çoğu zaman birbiriyle çelişkilidir. Adı İbn Hâcer el-Askalanî'nin eserinde Nesimüddin, Sıbt İbnü'l-Acemi'nin eserinde Ali ve diğer 
+      bazı kaynaklarda Celaleddin ve Ömer olarak geçmektir.[1] İmamüddin gerçek adı değil lakabıdır.[1]
+      </p>}
+      
+      <button onClick={warnButton}>warnMessage</button><br/>
+      <button onMouseOver={() => setIsVisible(true)}
+        onMouseOut={() => setIsVisible(false)}
+        
+
+      >hint</button>
+       {isVisible && <p>This is the paragraph content.</p>}
+    </div>
+  )
+}
+
+export default CustomButtom
